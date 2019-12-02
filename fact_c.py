f392744aecba771ef3892521b5e8d9a6f42a4d36
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-02 23:35:00 +0530

    Update fact_c.py

--- fact_c.py
+++ fact_c.py
@@ -2,8 +2,11 @@ from ctypes import *
 import time
 libpy = CDLL("./libpy.so")    #load .so file into object file
 n=input('Enter the integer')
-st_time=time.time()
-f=libpy.fact(n)
-end_time=time.time()
-print(f)
-print("time to run the function",end_time-st_time)
+if (n<0):
+	print('Invalid input. Enter positive integer')
+else:	
+	st_time=time.time()
+	f=float(libpy.fact(n))
+	end_time=time.time()
+	print(f)
+	print("time to run the function",end_time-st_time)
