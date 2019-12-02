cda4b649975f63d4b888b07f25832d2a40506445
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-02 23:32:07 +0530

    Update f_scipy.py

--- f_scipy.py
+++ f_scipy.py
@@ -1,8 +1,11 @@
 from scipy.special import factorial as f
 import time
 n=input('Enter the integer')
-st_time=time.time()
-f=f(n)
-end_time=time.time()
-print(f)
-print("time to run the function",end_time-st_time)
+if (n<0):
+	print('Input error. Enter a positive integer')
+else:
+	st_time=time.time()  
+	f=f(n)
+	end_time=time.time()
+	print(f)
+	print("time to run the function",end_time-st_time)
