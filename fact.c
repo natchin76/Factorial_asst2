433ba158eaad0d2107ab24308aa0271a6b355102
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-02 23:01:15 +0530

    Update fact.c

--- fact.c
+++ fact.c
@@ -2,7 +2,8 @@
 #include"fact.h"
 #include<time.h>   /*to calculate time taken to run the function*/
 int main(){
-int n,f;
+int n;
+double f;
 clock_t t;
 float time;
 printf("Enter an integer.:");
@@ -11,7 +12,7 @@ t=clock();     /*initializing t*/
 f=fact(n);
 t=clock()-t;   /*gives no. of clock cycles*/
 time=(float)t/CLOCKS_PER_SEC;    /*t gives no. of clock cycles, thus dividing by clocks per second*/
-printf("Factorial of %d is %d\n",n,f);
+printf("Factorial of %d is %.10e\n",n,f);
 printf("It took %.10e seconds to run the function\n",time);
 return(0);
 }
