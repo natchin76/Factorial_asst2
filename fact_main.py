adb25484cf95c74bb8499878f68a0cf6d18fa4f3
Chintan Patel <chintanpatel220598@gmail.com>
2019-12-02 23:36:05 +0530

    Update fact_main.py

--- fact_main.py
+++ fact_main.py
@@ -1,8 +1,11 @@
 import fact
 import time
 n=input('Enter the integer')
-st_time=time.time()  
-f=fact.f(n)
-end_time=time.time()
-print(f)
-print("time to run the function",end_time-st_time)
+if (n<0):
+	print('Input error. Enter a positive integer')
+else:
+	st_time=time.time()  
+	f=fact.f(n)
+	end_time=time.time()
+	print(f)
+	print("time to run the function",end_time-st_time)
